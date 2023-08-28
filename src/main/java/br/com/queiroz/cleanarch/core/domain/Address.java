d6a85e8153fd9fc11bd9d3ace5c32c2032bad410
Gabriel Queiroz <gabriel-cso@hotmail.com>
2023-08-27 22:05:35 -0300

    Created domain class

---
+++ src/main/java/br/com/queiroz/cleanarch/core/domain/Address.java
@@ -0,0 +1,42 @@
+package br.com.queiroz.cleanarch.core.domain;
+
+public class Address {
+
+    private String street;
+    private String city;
+    private String state;
+
+    public Address(){
+
+    }
+
+    public Address(String street, String city, String state){
+        this.street = street;
+        this.city = city;
+        this.state = state;
+    }
+
+    public String getStreet(){
+        return this.street;
+    }
+
+    public void setStreet(String street){
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
